2e30d557f5b53827a2dfb83cd2dd26208701199c
Josselin Massot <josselin.massot@polytechnique.edu>
2022-09-07 14:37:25 +0200

    fix code generator and add embedded methods

--- solver/code_generator.py
+++ solver/code_generator.py
@@ -3,11 +3,12 @@
 """code_generator.py
 
 Usage:
-    code_generator.py FILE...
+    code_generator.py FILE... (--output=<header>)
 
 Options:
-    -h, --help    show help
-    FILE          json file to split or join
+    -h, --help                      show help
+    FILE                            json file to split or join and name of output (last)
+    -o=<header>, --output=<header>  name of output file header C++
 
 Output is include/butcher_methods.hpp
 """
@@ -16,17 +17,31 @@ import json
 import os, sys
 import sympy as sp
 
-output_file = "include/solver/butcher_methods.hpp"
+def code_pow(x,e):
+  """
+    replace some trivial cases by a simpler expression (for code)
+    because all division in sympy are `sp.Pow(expr,-1)` and same
+    for every square, of square root
+  """
+  if e == 1 :
+    return x
+  if sp.S(e).is_number and e < 0 :
+    return 1./(code_pow(x,-e))
+  if e == sp.S.Half :
+    return sp.Function("std::sqrt",nargs=1)(x)
+  return sp.Function("std::pow",nargs=2)(x,e)
 
 def STLizer(expr):
   math_to_stl = [(f,sp.Function("std::"+str(f),nargs=1)) for f in (sp.sin,sp.cos,sp.exp)]
   math_to_stl.append( (sp.sqrt,sp.Function("std::sqrt",nargs=1)) )
 
+  #expr = 1.0*expr
+  expr = 1.0*expr
+
   for old,new in math_to_stl:
     expr = expr.subs(old,new)
   
-  std_pow = sp.Function("std::pow",nargs=2)
-  expr = expr.replace(sp.Pow,std_pow)
+  expr = expr.replace(sp.Pow,code_pow)
 
   return str(expr)
 
@@ -54,17 +69,27 @@ def extract_meth(filename):
       for i in range(0,nstages)
   ]) == 0 )
 
-  label = os.path.basename(filename).replace(".json","")
+  id = os.path.basename(filename).replace(".json","")
+  print(id)
   A = [ [ STLizer(sp.parse_expr(aij)) for aij in ai ] for ai in data['A'] ]
   b = [ STLizer(sp.parse_expr(bi)) for bi in data['b'] ]
   c = [ STLizer(sp.parse_expr(ci)) for ci in data['c'] ]
-  
-  return {
-    'label':label,
+
+  butcher = {
+    'label': data['label'],
+    'id': id,
     'A':[ " , ".join(ai) for ai in A ],
     'b':" , ".join(b),
     'c':" , ".join(c)
     }
+
+  if 'b2' in data:
+    b2 = [ STLizer(sp.parse_expr(bi)) for bi in data['b2'] ]
+    butcher['b2'] = " , ".join(b2)
+  
+  return butcher
+
+  
   
 if __name__ == '__main__':
     from docopt import docopt
@@ -73,11 +98,13 @@ if __name__ == '__main__':
 
     list_meth = [ extract_meth(f) for f in args['FILE'] if is_explicit(f) ]
 
+    local_dir = os.path.dirname(os.path.abspath(__file__))
+
     import jinja2
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(local_dir))
     template = env.get_template("tpl_butcher_methods.hxx")
 
-    with open(output_file,'w') as f:
+    with open(args['--output'],'w') as f:
       f.write(
         template.render(list_meth=list_meth)
       )
