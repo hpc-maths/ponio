aaae7a338f378683e7665477a013748a5072cc3b
Josselin Massot <josselin.massot@polytechnique.edu>
2022-09-09 13:27:14 +0200

    add argument to choose number of digit

--- solver/code_generator.py
+++ solver/code_generator.py
@@ -3,12 +3,13 @@
 """code_generator.py
 
 Usage:
-    code_generator.py FILE... (--output=<header>)
+    code_generator.py FILE... (--output=<header>) [--Ndigit=<N>]
 
 Options:
     -h, --help                      show help
     FILE                            json file to split or join and name of output (last)
     -o=<header>, --output=<header>  name of output file header C++
+    --Ndigit=<N>                    number of digit in output Butcher tableau [default: 36]
 
 Output is include/butcher_methods.hpp
 """
@@ -30,7 +31,7 @@ def is_explicit(filename):
   )
   return rk.is_explicit
 
-def extract_meth(filename):
+def extract_meth(filename,Ndigit=36):
   with open(filename,'r') as f:
     data = json.load(f)
 
@@ -43,9 +44,9 @@ def extract_meth(filename):
     b2=data['b2'] if 'b2' in data else None
   )
   
-  A = rk.A.evalf(n=36).tolist()
-  b = rk.b.evalf(n=36).T.tolist()[0]
-  c = rk.c.evalf(n=36).T.tolist()[0]
+  A = rk.A.evalf(n=Ndigit).tolist()
+  b = rk.b.evalf(n=Ndigit).T.tolist()[0]
+  c = rk.c.evalf(n=Ndigit).T.tolist()[0]
 
   butcher = {
     'label': data['label'],
@@ -57,7 +58,7 @@ def extract_meth(filename):
   }
 
   if 'b2' in data:
-    b2 = sp.Matrix(data['b2']).evalf(n=36).T.tolist()[0]
+    b2 = sp.Matrix(data['b2']).evalf(n=Ndigit).T.tolist()[0]
     butcher['b2'] = " , ".join(map(str,b2))
   
   return butcher
@@ -68,8 +69,9 @@ if __name__ == '__main__':
     from docopt import docopt
 
     args = docopt( __doc__ , sys.argv[1:] )
+    Ndigit=int(args['--Ndigit'])
 
-    list_meth = [ extract_meth(f) for f in args['FILE'] if is_explicit(f) ]
+    list_meth = [ extract_meth(f,Ndigit) for f in args['FILE'] if is_explicit(f) ]
 
     local_dir = os.path.dirname(os.path.abspath(__file__))
 
