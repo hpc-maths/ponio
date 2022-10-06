c3376e2c7719b947760fb3f9baf24e9b72b43f4d
Josselin Massot <josselin.massot@polytechnique.edu>
2022-10-06 17:51:53 +0200

    fix typo on Lotka-Volterra

--- solver/demos/lotka_volterra.py
+++ solver/demos/lotka_volterra.py
@@ -7,15 +7,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 X = np.linspace(0.9,1.8,10)
-filename_fmt = "example2/lokta_voltera_{:0.2f}.dat"
+filename_fmt = "example2/lotka_volterra_{:0.2f}.dat"
 
 os.makedirs("example2",exist_ok=True)
 
-make = subprocess.Popen(["make","lokta_voltera"])
+make = subprocess.Popen(["make","lotka_volterra"])
 make.wait()
 
 for x0 in X:
-  args = ["./lokta_voltera",filename_fmt.format(x0),str(x0)]
+  args = ["./lotka_volterra",filename_fmt.format(x0),str(x0)]
   process = subprocess.Popen(args)
 process.wait()
 
@@ -23,6 +23,6 @@ for x0 in X:
   data = np.loadtxt(filename_fmt.format(x0))
   plt.plot(data[:,1],data[:,2],"-",label=r"$x_0 = {:0.2f}$".format(x0))
 
-plt.title("Lokta-Voltera system")
+plt.title("Lotka-Volterra system")
 plt.legend()
 plt.show()
