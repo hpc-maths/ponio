01155c477abfd7de44b6f5b3915196cef69ea3b1
Josselin Massot <josselin.massot@polytechnique.edu>
2022-11-15 15:48:00 +0100

    add license

--- solver/include/solver/generic_butcher_rk.hpp
+++ solver/include/solver/generic_butcher_rk.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+// Copyright 2022 PONIO TEAM. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 #include <concepts>
 
 #include "stage.hpp"
