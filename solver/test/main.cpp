79b563094e5fdb93546ad4751a82718dcc0909d6
Josselin Massot <josselin.massot@polytechnique.edu>
2023-06-27 16:42:22 +0200

    update clang-format

--- solver/test/main.cpp
+++ solver/test/main.cpp
@@ -5,7 +5,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
-#include "detail.hxx"
-#include "observer.hxx"
-#include "test_order.hxx"
-#include "test_order_split.hxx"
+#include "detail.hxx"           // IWYU pragma: keep
+#include "observer.hxx"         // IWYU pragma: keep
+#include "test_order.hxx"       // IWYU pragma: keep
+#include "test_order_split.hxx" // IWYU pragma: keep
