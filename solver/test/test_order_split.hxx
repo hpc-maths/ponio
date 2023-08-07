460352f47847c6b4672521b7a560d851f90a83f9
Josselin Massot <josselin.massot@polytechnique.edu>
2023-08-07 12:45:59 +0200

    rename namespace

--- solver/test/test_order_split.hxx
+++ solver/test/test_order_split.hxx
@@ -4,31 +4,27 @@
 
 #pragma once
 
-#include <vector>
-#include <cmath>
 #include <algorithm>
+#include <cmath>
 #include <iostream>
+#include <vector>
 
 #include <doctest/doctest.h>
 
+#include <solver/runge_kutta.hpp>
 #include <solver/splitting.hpp>
-#include <solver/butcher_methods.hpp>
 
 #include "compute_order.hpp"
 
-TEST_CASE("detail::power")
+TEST_CASE( "detail::power" )
 {
-  auto lie_splitting = ode::splitting::make_lie_tuple(
-    std::make_pair(ode::butcher::rk_33(), .1),
-    std::make_pair(ode::butcher::rk_33(), .1),
-    std::make_pair(ode::butcher::rk_33(), .1)
-  );
-  auto strang_splitting = ode::splitting::make_strang_tuple(
-    std::make_pair(ode::butcher::rk_44(), .1),
-    std::make_pair(ode::butcher::rk_44(), .1),
-    std::make_pair(ode::butcher::rk_44(), .1)
-  );
+    auto lie_splitting    = ponio::splitting::make_lie_tuple( std::make_pair( ponio::runge_kutta::rk_33(), .1 ),
+        std::make_pair( ponio::runge_kutta::rk_33(), .1 ),
+        std::make_pair( ponio::runge_kutta::rk_33(), .1 ) );
+    auto strang_splitting = ponio::splitting::make_strang_tuple( std::make_pair( ponio::runge_kutta::rk_44(), .1 ),
+        std::make_pair( ponio::runge_kutta::rk_44(), .1 ),
+        std::make_pair( ponio::runge_kutta::rk_44(), .1 ) );
 
-  WARN( check_order(lie_splitting) == doctest::Approx(lie_splitting.order).epsilon(0.125) );
-  WARN( check_order(strang_splitting) == doctest::Approx(strang_splitting.order).epsilon(0.125) );
+    WARN( check_order( lie_splitting ) == doctest::Approx( lie_splitting.order ).epsilon( 0.125 ) );
+    WARN( check_order( strang_splitting ) == doctest::Approx( strang_splitting.order ).epsilon( 0.125 ) );
 }
