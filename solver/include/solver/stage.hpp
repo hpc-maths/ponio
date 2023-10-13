5cb55e5100f01b0253a128cf347ca5107490d77a
Josselin Massot <josselin.massot@polytechnique.edu>
2023-10-13 15:22:29 +0200

    dynamic number of stages ROCK

--- solver/include/solver/stage.hpp
+++ solver/include/solver/stage.hpp
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include <concepts>
+#include <limits>
 #include <type_traits>
 
 namespace ponio
@@ -14,5 +16,35 @@ namespace ponio
      */
     template <std::size_t s>
     using Stage = std::integral_constant<std::size_t, s>;
+
+    namespace stages
+    {
+        /**
+         * @brief constant to set dynamic number of stages in an algorithm
+         * @warning don't check if `N_stages` is lower than `ponio::stages::dynamic` because `std::numeric_limits<std::size_t>::infinity()
+         == 0`
+
+         */
+        static constexpr std::size_t dynamic = std::numeric_limits<std::size_t>::infinity();
+
+        /**
+         * @brief test is algorithm has a static number of stages
+         *
+         * @tparam Algorithm_t algorithm (Runge-Kutta method) to check
+         */
+        template <typename Algorithm_t>
+        concept has_static_number_of_stages = Algorithm_t::N_stages !=
+        dynamic;
+
+        /**
+         * @brief test is algorithm has a dynamic number of stages
+         *
+         * @tparam Algorithm_t algorithm (Runge-Kutta method) to check
+         */
+        template <typename Algorithm_t>
+        concept has_dynamic_number_of_stages = !
+        has_static_number_of_stages<Algorithm_t>;
+
+    }
 
 }
