106ddd5453e8f36655b5b6916cd37b183bbd3535
Josselin Massot <josselin.massot@polytechnique.edu>
2023-12-05 10:54:14 +0100

    remove xtensor include

--- solver/demos/heat_samurai.cpp
+++ solver/demos/heat_samurai.cpp
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#include <xtensor/xfixed.hpp>
-
 #include <samurai/algorithm.hpp>
 #include <samurai/bc.hpp>
 #include <samurai/field.hpp>
