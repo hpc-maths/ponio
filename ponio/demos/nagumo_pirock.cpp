6a238aab43fbe40079c6b08ddedebb5e8b00360c
Josselin Massot <josselin.massot@polytechnique.edu>
2024-03-04 18:09:52 +0100

    add first PIROCK implementation

---
+++ ponio/demos/nagumo_pirock.cpp
@@ -0,0 +1,219 @@
+// Copyright 2022 PONIO TEAM. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include <iostream>
+#include <valarray>
+
+#include <algorithm>
+#include <filesystem>
+#include <fstream>
+#include <iterator>
+#include <numbers>
+#include <sstream>
+
+#include <ponio/observer.hpp>
+#include <ponio/runge_kutta.hpp>
+#include <ponio/runge_kutta/pirock.hpp>
+#include <ponio/samurai_linear_algebra.hpp>
+#include <ponio/solver.hpp>
+#include <ponio/time_span.hpp>
+
+#include <samurai/field.hpp>
+#include <samurai/hdf5.hpp>
+#include <samurai/mr/adapt.hpp>
+#include <samurai/mr/mesh.hpp>
+#include <samurai/samurai.hpp>
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
+template <class field_t>
+void
+save( fs::path const& path, std::string const& filename, field_t& u, std::string const& suffix = "" )
+{
+    auto mesh   = u.mesh();
+    auto level_ = samurai::make_field<std::size_t, 1>( "level", mesh );
+    u.name()    = "u";
+
+    if ( !fs::exists( path ) )
+    {
+        fs::create_directory( path );
+    }
+
+    samurai::for_each_cell( mesh,
+        [&]( auto& cell )
+        {
+            level_[cell] = cell.level;
+        } );
+
+    samurai::save( path, fmt::format( "{}{}", filename, suffix ), mesh, u, level_ );
+}
+
+template <typename f_explicit_type, typename f_implicit_type, typename f_implicit_t_type>
+struct pirock_pb
+{
+    f_explicit_type f_explicit;
+    f_implicit_type f_implicit;
+    f_implicit_t_type f_implicit_t;
+
+    pirock_pb( f_explicit_type&& f_explicit_, f_implicit_type&& f_implicit_, f_implicit_t_type&& f_implicit_t_ )
+        : f_explicit( f_explicit_ )
+        , f_implicit( f_implicit_ )
+        , f_implicit_t( f_implicit_t_ )
+    {
+    }
+
+    // template <typename state_t>
+    // auto
+    // operator()( double t, state_t&& u )
+    // {
+    //     return fd( t, u ) + fr( t, u );
+    // }
+};
+
+template <typename fd_type, typename fr_type, typename fr_t_type>
+auto
+make_pirock_pb( fd_type&& fd, fr_type&& fr, fr_t_type&& fr_t )
+{
+    return pirock_pb<fd_type, fr_type, fr_t_type>( std::forward<fd_type>( fd ), std::forward<fr_type>( fr ), std::forward<fr_t_type>( fr_t ) );
+}
+
+int
+main( int argc, char** argv )
+{
+    PetscInitialize( &argc, &argv, 0, nullptr );
+
+    constexpr std::size_t dim = 1; // cppcheck-suppress unreadVariable
+    using config_t            = samurai::MRConfig<dim>;
+    using box_t               = samurai::Box<double, dim>;
+    using point_t             = typename box_t::point_t;
+
+    // simulation parameters --------------------------------------------------
+    constexpr double d = .1;
+    constexpr double k = 1. / d;
+
+    constexpr double left_box  = -40;
+    constexpr double right_box = 10;
+    constexpr double t_ini     = 0.;
+    constexpr double t_end     = 35.;
+
+    // multiresolution parameters
+    std::size_t min_level = 0;
+    std::size_t max_level = 6;
+    double mr_epsilon     = 1e-5; // Threshold used by multiresolution
+    double mr_regularity  = 1.;   // Regularity guess for multiresolution
+
+    // output parameters
+    std::string const dirname = "nagumo_pirock_data";
+    fs::path path             = std::filesystem::path( dirname );
+    std::string filename      = "u";
+    fs::create_directories( path );
+
+    // define mesh
+    point_t box_corner1, box_corner2;
+    box_corner1.fill( left_box );
+    box_corner2.fill( right_box );
+    box_t box( box_corner1, box_corner2 );
+    samurai::MRMesh<config_t> mesh{ box, min_level, max_level };
+
+    // init solution ----------------------------------------------------------
+    auto u_ini = samurai::make_field<1>( "u", mesh );
+
+    auto exact_solution = [&]( double x, double t )
+    {
+        double x0  = -25.;
+        double v   = ( 1. / std::sqrt( 2. ) ) * std::sqrt( k * d );
+        double cst = -( 1. / std::sqrt( 2. ) ) * std::sqrt( k / d );
+        double e   = std::exp( cst * ( x - x0 - v * t ) );
+        return e / ( 1. + e );
+    };
+
+    samurai::for_each_cell( mesh,
+        [&]( auto& cell )
+        {
+            u_ini[cell] = exact_solution( cell.center( 0 ), 0 );
+        } );
+    samurai::make_bc<samurai::Neumann>( u_ini, 0. );
+
+    // define problem ---------------------------------------------------------
+
+    // diffusion terme
+    auto diff = samurai::make_diffusion_order2<decltype( u_ini )>( d );
+    auto fd   = [&]( double /* t */, auto&& u )
+    {
+        samurai::make_bc<samurai::Neumann>( u, 0. );
+        samurai::update_ghost_mr( u );
+        return -diff( u );
+    };
+
+    // reaction terme
+    using cfg  = samurai::LocalCellSchemeConfig<samurai::SchemeType::NonLinear, 1, decltype( u_ini )>;
+    auto react = samurai::make_cell_based_scheme<cfg>();
+    react.set_name( "Reaction" );
+    react.set_scheme_function(
+        [&]( auto const& cell, auto const& field )
+        {
+            auto u = field[cell];
+            return k * u * u * ( 1 - u );
+        } );
+    react.set_jacobian_function(
+        [&]( auto const& cell, auto const& field )
+        {
+            auto u = field[cell];
+            return k * ( 2 * u * ( 1 - u ) - u * u );
+        } );
+    auto fr_t = [&]( double /* t */ )
+    {
+        return react;
+    };
+    auto fr = [&]( double t, auto&& u )
+    {
+        samurai::make_bc<samurai::Neumann>( u, 0. );
+        samurai::update_ghost_mr( u );
+        return fr_t( t )( u );
+    };
+
+    auto pb = make_pirock_pb( fd, fr, fr_t );
+
+    ponio::time_span<double> const tspan = { t_ini, t_end };
+    double dt                            = ( t_end - t_ini ) / 2000;
+
+    // time loop  -------------------------------------------------------------
+    auto sol_range = ponio::make_solver_range( pb, ponio::runge_kutta::pirock::pirock<13>(), u_ini, tspan, dt );
+
+    auto it_sol = sol_range.begin();
+
+    // preapre MR for solution on iterator
+    auto mr_adaptation = samurai::make_MRAdapt( it_sol->state );
+    samurai::make_bc<samurai::Neumann>( it_sol->state, 0. );
+    mr_adaptation( mr_epsilon, mr_regularity );
+    samurai::update_ghost_mr( it_sol->state );
+
+    std::size_t n_save = 0;
+    save( path, filename, it_sol->state, fmt::format( "_ite_{}", n_save++ ) );
+
+    while ( it_sol->time < t_end )
+    {
+        // samurai::make_bc<samurai::Neumann>( it_sol->state, 0. );
+        //  TODO: add a callback function to make this before each iteration
+        for ( auto& ki : it_sol.meth.kis )
+        {
+            ki.resize();
+            ki.fill( 0. );
+        }
+
+        ++it_sol;
+        std::cout << "tⁿ: " << std::setw( 8 ) << it_sol->time << " (Δt: " << it_sol->time_step << ") " << n_save << "\r";
+
+        mr_adaptation( mr_epsilon, mr_regularity );
+        samurai::update_ghost_mr( it_sol->state );
+
+        save( path, filename, it_sol->state, fmt::format( "_ite_{}", n_save++ ) );
+    }
+    std::cout << std::endl;
+
+    PetscFinalize();
+
+    return 0;
+}
---
+++ ponio/include/ponio/runge_kutta/pirock.hpp
@@ -0,0 +1,497 @@
+// Copyright 2022 PONIO TEAM. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#pragma once
+#include <concepts>
+#include <numeric>
+#include <ranges>
+#include <string_view>
+#include <type_traits>
+
+#include "../butcher_tableau.hpp"
+#include "../detail.hpp"
+#include "../linear_algebra.hpp"
+#include "../ponio_config.hpp"
+#include "../stage.hpp"
+#include "rock.hpp"
+
+namespace ponio::runge_kutta::pirock
+{
+    /*
+    template <typename value_t = double>
+    struct piheun
+    {
+        static constexpr bool is_embedded     = false;
+        static constexpr std::size_t N_stages = 5;
+
+        value_t delta;
+        value_t gamma;
+        value_t beta;
+
+        piheun()
+            : delta( 1.0 )
+            , gamma( 1.0 )
+            , beta( 1.0 )
+        {
+        }
+
+        template <typename problem_t, typename state_t, typename array_ki_t, std::size_t j>
+        inline state_t
+        stage( Stage<0>, problem_t& pb, value_t tn, state_t& un, array_ki_t const& Ki, value_t dt )
+        {
+            return un + dt * pb.F_d( un );
+        }
+
+        template <typename problem_t, typename state_t, typename array_ki_t, std::size_t j>
+        inline state_t
+        stage( Stage<1>, problem_t& pb, value_t tn, state_t& un, array_ki_t const& Ki, value_t dt )
+        {
+            return un + dt * delta * pb.F_d( un );
+        }
+
+        template <typename problem_t, typename state_t, typename array_ki_t, std::size_t j>
+        inline state_t
+        stage( Stage<2>, problem_t& pb, value_t tn, state_t& un, array_ki_t const& Ki, value_t dt )
+        {
+            // TODO: Newton method
+            auto g = [&]( k3 )
+            {
+                return k3 - ( Ki[1] + dt * gamma * pb.F_r( k3 ) );
+            };
+
+            auto dg = [&]( k3 )
+            {
+                return Identity - dt * gamma * pb.JF_r( k3 );
+            };
+
+            // ....
+
+            return Ki[1] + dt * gamma * pb.F_r( Ki[2] );
+        }
+
+        template <typename problem_t, typename state_t, typename array_ki_t, std::size_t j>
+        inline state_t
+        stage( Stage<3>, problem_t& pb, value_t tn, state_t& un, array_ki_t const& Ki, value_t dt )
+        {
+            // TODO: Newton method
+            auto g = [&]( k4 )
+            {
+                return k4 - ( Ki[1] + dt * beta * pb.F_d( Ki[2] ) + ( 1 - 2 * gamma ) * dt * pb.F_r( Ki[2] ) + gamma * dt * pb.F_r( k4 ) );
+            };
+
+            auto Jg = [&]( k4 )
+            {
+                return Identity - gamma * dt * pb.JF_r( k4 );
+            };
+
+            // ....
+
+            return Ki[1] + dt * beta * pb.F_d( Ki[2] ) + ( 1 - 2 * gamma ) * dt * pb.F_r( Ki[2] ) + gamma * dt * pb.F_r( Ki[3] );
+        }
+
+        template <typename problem_t, typename state_t, typename array_ki_t, std::size_t j>
+        inline state_t
+        stage( Stage<4>, problem_t& pb, value_t tn, state_t& un, array_ki_t const& Ki, value_t dt )
+        {
+            return Ki[1] + dt * ( 1.0 - gamma ) * pb.F_r( Ki[2] );
+        }
+
+        template <typename problem_t, typename state_t, typename array_ki_t, std::size_t j>
+        inline state_t
+        stage( Stage<5>, problem_t& pb, value_t tn, state_t& un, array_ki_t const& Ki, value_t dt )
+        {
+            return un + 0.5 * dt * pb.F_d( Ki[0] ) + 0.5 * dt * pb.F_d( Ki[1] )
+                 + dt / ( 2.0 - 4.0 * gamma ) * ( pb.F_d( Ki[4] ) - pb.F_d( Ki[2] ) ) + 0.5 * dt * pb.F_r( Ki[2] )
+                 + 0.5 * dt * pb.F_r( Ki[3] );
+        }
+    };
+    */
+
+    namespace polynomial
+    {
+        template <std::size_t N, typename value_t = double>
+        struct polynomial
+        {
+            static constexpr std::size_t degree = N;
+
+            std::array<value_t, degree + 1> coeff;
+
+            constexpr double
+            operator[]( std::size_t idx ) const
+            {
+                if ( idx < coeff.size() )
+                {
+                    return coeff[idx];
+                }
+                else
+                {
+                    return static_cast<value_t>( 0. );
+                }
+            }
+
+            template <std::size_t M>
+            polynomial<N, value_t>&
+            operator=( polynomial<M, value_t> const& rhs )
+            {
+                for ( std::size_t i = 0; i < coeff.size(); ++i )
+                {
+                    coeff[i] = rhs[i];
+                }
+
+                return *this;
+            }
+
+            template <std::size_t... Is>
+            constexpr value_t
+            eval_impl( value_t&& x, std::index_sequence<Is...> ) const
+            {
+                return ( 0. + ... + ( coeff[Is] * detail::power<Is>( std::forward<value_t>( x ) ) ) );
+            }
+
+            constexpr value_t
+            operator()( value_t&& x ) const
+            {
+                return eval_impl( std::forward<value_t>( x ), std::make_index_sequence<degree + 1>{} );
+            }
+        };
+
+        template <std::size_t N, std::size_t M, typename value_t>
+        constexpr polynomial<std::max( N, M ), value_t> const&
+        max_degree( polynomial<N, value_t> const& P, polynomial<M, value_t> const& Q )
+        {
+            if constexpr ( N >= M )
+            {
+                return P;
+            }
+            else
+            {
+                return Q;
+            }
+        }
+
+        template <std::size_t N, std::size_t M, typename value_t>
+        constexpr polynomial<std::min( N, M ), value_t> const&
+        min_degree( polynomial<N, value_t> const& P, polynomial<M, value_t> const& Q )
+        {
+            if constexpr ( N >= M )
+            {
+                return Q;
+            }
+            else
+            {
+                return P;
+            }
+        }
+
+        /* operator + (polynomial, polynomial) */
+        template <std::size_t N, std::size_t M, typename value_t, std::size_t... Is, std::size_t... Js>
+        constexpr polynomial<N, value_t>
+        add_poly_poly( polynomial<N, value_t> const& P, polynomial<M, value_t> const& Q, std::index_sequence<Is...>, std::index_sequence<Js...> )
+        {
+            return {
+                {( P[Is] + Q[Is] )..., P[Js + M]...}
+            };
+        }
+
+        template <std::size_t N, std::size_t M, typename value_t>
+        polynomial<std::max( N, M ), value_t>
+        operator+( polynomial<N, value_t> const& P, polynomial<M, value_t> const& Q )
+        {
+            return add_poly_poly( max_degree( P, Q ),
+                min_degree( P, Q ),
+                std::make_index_sequence<std::min( N, M ) + 1>{},
+                std::make_index_sequence<std::max( N, M ) - std::min( N, M )>{} );
+        }
+
+        /* operator - (polynomial, polynomial) */
+        template <std::size_t N, std::size_t M, typename value_t>
+        polynomial<std::max( N, M ), value_t>
+        operator-( polynomial<N, value_t> const& P, polynomial<M, value_t> const& Q )
+        {
+            return P + ( -1. ) * Q;
+        }
+
+        /* operator * (value_t, polynomial) */
+        template <std::size_t N, typename value_t, std::size_t... Is>
+        constexpr polynomial<N, value_t>
+        mul_scalar_poly( value_t a, polynomial<N, value_t> const& P, std::index_sequence<Is...> )
+        {
+            return polynomial<N, value_t>( { { ( a * P[Is] )... } } );
+        }
+
+        template <std::size_t N, typename value_t>
+        polynomial<N, value_t>
+        operator*( value_t a, polynomial<N, value_t> const& P )
+        {
+            return mul_scalar_poly( a, P, std::make_index_sequence<N + 1>{} );
+        }
+
+        /* operator * (polynomial, polynomial) */
+        template <std::size_t K, std::size_t N, std::size_t M, typename value_t, std::size_t... Is>
+        constexpr value_t
+        mul_poly_poly_coeff( polynomial<N, value_t> const& P, polynomial<M, value_t> const& Q, std::index_sequence<Is...> )
+        {
+            return ( static_cast<value_t>( 0. ) + ... + ( P[Is] * Q[K - Is] ) );
+        }
+
+        template <std::size_t N, std::size_t M, typename value_t, std::size_t... Is>
+        constexpr polynomial<N + M, value_t>
+        mul_poly_poly( polynomial<N, value_t> const& P, polynomial<M, value_t> const& Q, std::index_sequence<Is...> )
+        {
+            return { { mul_poly_poly_coeff<Is>( P, Q, std::make_index_sequence<N + M>{} )... } };
+        }
+
+        template <std::size_t N, std::size_t M, typename value_t>
+        constexpr polynomial<N + M, value_t>
+        operator*( polynomial<N, value_t> const& P, polynomial<M, value_t> const& Q )
+        {
+            return mul_poly_poly( max_degree( P, Q ), min_degree( P, Q ), std::make_index_sequence<N + M + 1>{} );
+        }
+
+        /* derivation */
+        template <std::size_t N, typename value_t, std::size_t... Is>
+        constexpr polynomial<std::max( 0ul, N - 1 ), value_t>
+        deriv_impl( polynomial<N, value_t> const& P, std::index_sequence<Is...> )
+        {
+            return { { ( ( Is + 1 ) * P[Is + 1] )... } };
+        }
+
+        template <std::size_t N, typename value_t>
+        constexpr polynomial<std::max( 0ul, N - 1 ), value_t>
+        deriv( polynomial<N, value_t> const& P )
+        {
+            return deriv_impl( P, std::make_index_sequence<N>{} );
+        }
+
+        /* display */
+        template <std::size_t N, typename value_t>
+        std::ostream&
+        operator<<( std::ostream& os, polynomial<N, value_t> const& P )
+        {
+            os << P[0];
+            if constexpr ( N > 0 )
+            {
+                for ( std::size_t i = 0; i < P.coeff.size(); ++i )
+                {
+                    std::cout << " + " << P[i] << "*X";
+                    if ( i > 1 )
+                    {
+                        std::cout << "**" << i;
+                    }
+                }
+            }
+        }
+
+        template <std::size_t s, typename rock_coeff>
+        std::pair<std::size_t, std::size_t>
+        optimal_degree()
+        {
+            std::size_t mdeg = s - 2;
+            std::size_t mz = 1, mr = 1;
+
+            if ( mdeg < 2 )
+            {
+                return { mz, mr };
+            }
+
+            std::size_t i = 1;
+            for ( auto ms_i : rock_coeff::ms )
+            {
+                if ( ms_i / mdeg >= 1 )
+                {
+                    mdeg = rock_coeff::ms[i - 1];
+                    mz   = i;
+                    break;
+                }
+                mr = mr + rock_coeff::ms[i - 1] * 2 - 1;
+
+                ++i;
+            }
+
+            return { mz, mr };
+        }
+
+        template <std::size_t s, std::size_t l, typename value_t>
+        polynomial<s - 2 + l>
+        P_sm2pl()
+        {
+            using rock_coeff = rock::rock2_coeff<value_t>;
+
+            polynomial<1, value_t> z   = { 0., 1. };
+            polynomial<0, value_t> one = { 1. };
+
+            auto f = [=]( auto u )
+            {
+                return z * u;
+            };
+
+            std::size_t mdeg = s - 2;
+            auto [mz, mr] = optimal_degree<s, rock_coeff>(); // rock::detail::degree_computer<value_t, rock_coeff>::optimal_degree( mdeg );
+
+            polynomial<s - 2 + l, value_t> u;
+            polynomial<s - 2 + l, value_t> ujm1;
+            polynomial<s - 2 + l, value_t> ujm2;
+
+            u    = one;
+            ujm2 = one;
+
+            auto mu_1 = rock_coeff::recf[mr - 1];
+            ujm1      = u + mu_1 * f( u );
+
+            if constexpr ( s <= 2 )
+            {
+                u = ujm1;
+            }
+
+            for ( std::size_t j = 2; j < s - 2 + l + 1; ++j )
+            {
+                double const mu    = rock_coeff::recf[mr + 2 * ( j - 2 ) + 1 - 1];
+                double const kappa = rock_coeff::recf[mr + 2 * ( j - 2 ) + 2 - 1];
+                double const nu    = -1.0 - kappa;
+
+                u = mu * f( ujm1 ) - nu * ujm1 - kappa * ujm2;
+
+                ujm2 = ujm1;
+                ujm1 = u;
+            }
+
+            return u;
+        }
+
+        template <std::size_t s, std::size_t l, typename value_t = double>
+        value_t
+        Pp_sm2pl_0()
+        {
+            return deriv( P_sm2pl<s, l, value_t>() )( 0. );
+        }
+
+    } // namespace polynomial
+
+    /** @class pirock_impl
+     *  @brief define PIROCK method
+     *
+     *  @warning the implementation is only with l=1 and beta=0 with a fixed number of stages
+     */
+    template <std::size_t _s, typename value_t = double>
+    struct pirock
+    {
+        static constexpr bool is_embedded      = false;
+        static constexpr std::size_t N_stages  = stages::dynamic;
+        static constexpr std::size_t N_storage = 10;
+
+        static constexpr std::size_t s = _s;
+        static constexpr std::size_t l = 1;
+
+        using rock_coeff      = rock::rock2_coeff<value_t>;
+        using degree_computer = rock::detail::degree_computer<value_t, rock_coeff>;
+
+        // eig_computer_t eig_computer_t;
+        value_t alpha;
+
+        pirock( value_t alpha_ )
+            : alpha( alpha_ )
+        {
+        }
+
+        template <typename problem_t, typename state_t, typename array_ki_t>
+        inline std::tuple<value_t, state_t, value_t>
+        operator()( problem_t& pb, value_t& tn, state_t& un, array_ki_t& U, value_t& dt )
+        {
+            // TODO: change here s and mdeg computation to compute this from pb.f_explicit
+            std::size_t mdeg              = s - 2;
+            auto [deg_index, start_index] = polynomial::optimal_degree<s, rock_coeff>();
+
+            // TODO: change this polynomial into a runtime computation
+            value_t const beta  = 1. - 2. * alpha * polynomial::Pp_sm2pl_0<s, l, value_t>();
+            value_t const gamma = 1. - 0.5 * std::sqrt( 2. );
+
+            auto& u_j   = U[0];
+            auto& u_jm1 = U[1];
+            auto& u_jm2 = U[2];
+            auto& u_sm2 = U[3];
+
+            u_j   = un;
+            u_jm2 = un;
+
+            value_t const mu_1 = rock_coeff::recf[start_index - 1];
+
+            value_t t_jm1 = tn + dt * alpha * mu_1;
+            value_t t_jm2 = tn + dt * alpha * mu_1;
+            value_t t_jm3 = tn;
+
+            u_jm1 = un + alpha * dt * mu_1 * pb.f_explicit( tn, un );
+
+            if ( mdeg < 2 )
+            {
+                u_j = u_jm1;
+            }
+
+            for ( std::size_t j = 2; j < s - 2 + l + 1; ++j )
+            {
+                value_t const mu_j    = rock_coeff::recf[start_index + 2 * ( j - 2 ) + 1 - 1];
+                value_t const kappa_j = rock_coeff::recf[start_index + 2 * ( j - 2 ) + 2 - 1];
+                value_t const nu_j    = -1.0 - kappa_j;
+
+                u_j = alpha * dt * mu_j * pb.f_explicit( t_jm1, u_jm1 ) - nu_j * u_jm1 - kappa_j * u_jm2;
+
+                t_jm1 = alpha * dt * mu_j - nu_j * t_jm2 - kappa_j * t_jm3;
+
+                if ( j == s - 2 )
+                {
+                    u_sm2 = u_j;
+                }
+                if ( j < s - 2 + l )
+                {
+                    std::swap( u_jm2, u_jm1 );
+                    std::swap( u_jm1, u_j );
+                }
+
+                t_jm3 = t_jm2;
+                t_jm2 = t_jm1;
+            }
+            // u_j -> u_{s-2+l} = u_{s-1}
+            // u_jm1 -> u_{s-2+l-1} = u_{s-2}
+            // u_jm2 -> u_{s-2+l-2} = u_{s-3}
+
+            value_t sigma   = rock_coeff::fp1[deg_index - 1];
+            value_t sigma_a = 0.5 * ( 1.0 - alpha ) + alpha * sigma;
+            auto& us_sm1    = U[4];
+            us_sm1          = u_sm2 + sigma_a * dt * pb.f_explicit( t_jm1, u_sm2 );
+
+            auto& us_s = U[5];
+            us_s       = us_sm1 + sigma_a * dt * pb.f_explicit( t_jm1, us_sm1 );
+
+            auto& u_sm2pl = u_j;
+
+            auto& u_sp1 = U[6];
+            u_sp1       = u_sm2pl;
+
+            auto op_sp1  = ::ponio::linear_algebra::operator_algebra<state_t>::identity( un ) - gamma * dt * pb.f_implicit_t( tn );
+            auto rhs_sp1 = u_sm2pl;
+            ::ponio::linear_algebra::operator_algebra<state_t>::solve( op_sp1, u_sp1, rhs_sp1 );
+
+            auto& u_sp2  = U[7];
+            u_sp2        = u_sm2pl;
+            auto op_sp2  = ::ponio::linear_algebra::operator_algebra<state_t>::identity( un ) - gamma * dt * pb.f_implicit_t( tn );
+            auto rhs_sp2 = static_cast<state_t>(
+                u_sm2pl + beta * pb.f_explicit( tn, u_sp1 ) + ( 1. - 2. * gamma ) * dt * pb.f_implicit( tn, u_sp1 ) );
+            ::ponio::linear_algebra::operator_algebra<state_t>::solve( op_sp2, u_sp2, rhs_sp2 );
+
+            auto& u_sp3 = U[8];
+            u_sp3       = u_sm2pl + ( 1. - gamma ) * dt * pb.f_implicit( tn, u_sp1 );
+
+            value_t tau   = sigma * rock_coeff::fp2[deg_index - 1] + sigma * sigma;
+            value_t tau_a = 0.5 * detail::power<2>( alpha - 1. ) + 2. * alpha * ( 1. - alpha ) * sigma + alpha * alpha * tau;
+
+            auto& u_np1 = U[9];
+            u_np1       = us_s
+                  - sigma_a * ( 1. - tau_a / ( sigma_a * sigma_a ) ) * dt * ( pb.f_explicit( tn, us_sm1 ) - pb.f_explicit( tn, u_sm2 ) )
+                  + 0.5 * dt * pb.f_implicit( tn, u_sp1 ) + 0.5 * dt * pb.f_implicit( tn, u_sp2 )
+                  + 1. / ( 2. - 4. * gamma ) * dt * ( pb.f_explicit( tn, u_sp3 ) - pb.f_explicit( tn, u_sp1 ) );
+
+            return { tn + dt, u_np1, dt };
+        }
+    };
+
+} // namespace ponio::runge_kutta::pirock
