5a7df884f9caed2257102122d7e35a8b186760c4
Josselin Massot <josselin.massot@polytechnique.edu>
2024-09-19 16:22:11 +0200

    fix IWYU

--- ponio/include/ponio/runge_kutta/rock.hpp
+++ ponio/include/ponio/runge_kutta/rock.hpp
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// IWYU pragma: private, include "../runge_kutta.h"
+// IWYU pragma: private, include "../runge_kutta.hpp"
 
 #pragma once
 
