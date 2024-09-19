bc527634b0f2e460af6c523037e1e26ff4df1249
Josselin Massot <josselin.massot@polytechnique.edu>
2024-09-19 16:22:47 +0200

    add test of number_of_eval for strang methods

--- ponio/test/iteration_info.hxx
+++ ponio/test/iteration_info.hxx
@@ -159,12 +159,89 @@ TEST_CASE( "number_of_eval::splitting_lie" )
     CHECK( cumulative_counter_2 == manual_counter_2 );
 }
 
-// TEST_CASE("number_of_eval::splitting_strang")
-// {
+TEST_CASE( "number_of_eval::splitting_strang" )
+{
+    std::size_t manual_counter_1 = 0;
+    std::size_t manual_counter_2 = 0;
 
-// }
+    double const k = 50;
+    auto f1        = [&]( double, double y )
+    {
+        ++manual_counter_1;
+        return k * y;
+    };
+    auto f2 = [&, k]( double t, double )
+    {
+        ++manual_counter_2;
+        return k * std::cos( t );
+    };
 
-// TEST_CASE("number_of_eval::splitting_adaptive_strang")
-// {
+    double y_0 = 2.0;
 
-// }
+    ponio::time_span<double> const t_span = { 0., 2. };
+    double const dt                       = 0.05;
+
+    auto curtiss_hirschfelder = ponio::make_problem( f1, f2 );
+    auto strang               = ponio::splitting::make_strang_tuple( std::make_pair( ponio::runge_kutta::rk_33(), 0.125 * dt ),
+        std::make_pair( ponio::runge_kutta::rk_33(), 0.25 * dt ) );
+    auto sol_range            = ponio::make_solver_range( curtiss_hirschfelder, strang, y_0, t_span, dt );
+    auto it_sol               = sol_range.begin();
+
+    std::size_t cumulative_counter_1 = 0;
+    std::size_t cumulative_counter_2 = 0;
+    while ( it_sol->time < t_span.back() )
+    {
+        ++it_sol;
+        cumulative_counter_1 += it_sol.info().number_of_eval[0];
+        cumulative_counter_2 += it_sol.info().number_of_eval[1];
+    }
+
+    CHECK( cumulative_counter_1 == manual_counter_1 );
+    CHECK( cumulative_counter_2 == manual_counter_2 );
+}
+
+TEST_CASE( "number_of_eval::splitting_adaptive_strang" )
+{
+    std::size_t manual_counter_1 = 0;
+    std::size_t manual_counter_2 = 0;
+
+    double const k = 50;
+    auto f1        = [&]( double, double y )
+    {
+        ++manual_counter_1;
+        return k * y;
+    };
+    auto f2 = [&, k]( double t, double )
+    {
+        ++manual_counter_2;
+        return k * std::cos( t );
+    };
+
+    double y_0 = 2.0;
+
+    ponio::time_span<double> const t_span = { 0., 2. };
+    double const dt                       = 0.05;
+
+    auto curtiss_hirschfelder = ponio::make_problem( f1, f2 );
+
+    double delta         = 5e-3;
+    double tol           = 1e-3;
+    auto adaptive_strang = ponio::splitting::make_adaptive_strang_tuple( delta,
+        tol,
+        std::make_pair( ponio::runge_kutta::rk_33(), 0.125 * dt ),
+        std::make_pair( ponio::runge_kutta::rk_33(), 0.25 * dt ) );
+    auto sol_range       = ponio::make_solver_range( curtiss_hirschfelder, adaptive_strang, y_0, t_span, dt );
+    auto it_sol          = sol_range.begin();
+
+    std::size_t cumulative_counter_1 = 0;
+    std::size_t cumulative_counter_2 = 0;
+    while ( it_sol->time < t_span.back() )
+    {
+        ++it_sol;
+        cumulative_counter_1 += it_sol.info().number_of_eval[0];
+        cumulative_counter_2 += it_sol.info().number_of_eval[1];
+    }
+
+    CHECK( cumulative_counter_1 == manual_counter_1 );
+    CHECK( cumulative_counter_2 == manual_counter_2 );
+}
