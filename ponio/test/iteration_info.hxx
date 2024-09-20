f74baccb7784b78c8be81c13f77a263921868c03
Josselin Massot <josselin.massot@polytechnique.edu>
2024-09-20 19:53:47 +0200

    add coments and fix sign in Curtiss Hirschfelder problem

--- ponio/test/iteration_info.hxx
+++ ponio/test/iteration_info.hxx
@@ -13,6 +13,34 @@
 #include <ponio/solver.hpp>
 #include <ponio/splitting.hpp>
 
+/**
+ * In this test case we solve the Curtiss and Hirschfelder problem:
+ *
+ * \f$$
+ *  \begin{aligned}
+ *    \dot{y} = k(\cos(t) - y) \\
+ *    y(0) = 2
+ *  \end{aligned}
+ * \f$$
+ *
+ * We solve this equation with a represent of each class of numerical integrator, simply to test if number of evaluation of function is
+ * correctly computing.
+ *
+ */
+
+/**
+ * ----------------------------------------------------------------------------
+ *
+ * # Explicit Runge-Kutta method
+ *
+ * This class of methods are writing to solve a problem with form as
+ *
+ * \f$$
+ *  \dot{y} = f(t, y)
+ * \f$$
+ *
+ * with simply write the function `curtiss_hirschfelder` for this.
+ */
 TEST_CASE( "number_of_eval::explicit_runge_kutta" )
 {
     std::size_t manual_counter = 0;
@@ -21,7 +49,7 @@ TEST_CASE( "number_of_eval::explicit_runge_kutta" )
     auto curtiss_hirschfelder = [&, k]( double t, double y )
     {
         ++manual_counter;
-        return k * ( y + std::cos( t ) );
+        return k * ( std::cos( t ) - y );
     };
 
     double y_0 = 2.0;
@@ -42,11 +70,26 @@ TEST_CASE( "number_of_eval::explicit_runge_kutta" )
     CHECK( cumulative_counter == manual_counter );
 }
 
+/**
+ * ----------------------------------------------------------------------------
+ *
+ * # Lawson and exponential Runge-Kutta methods
+ *
+ * This classes of methods are writing to solve a problem with form as
+ *
+ * \f$$
+ *  \dot{y} = Ly + N(t, y)
+ * \f$$
+ *
+ * with define \f$L=-k\f$ and \f$N:t, y\mapsto k\cos(t)\f$, we build an object `curtiss_hirschfelder` to keep link between two mathematic
+ * objects.
+ */
 TEST_CASE( "number_of_eval::lawson_runge_kutta" )
 {
     std::size_t manual_counter = 0;
 
     double const k = 50;
+    double const l = -k;
     auto n         = [&, k]( double t, double )
     {
         ++manual_counter;
@@ -58,7 +101,7 @@ TEST_CASE( "number_of_eval::lawson_runge_kutta" )
     ponio::time_span<double> const t_span = { 0., 2. };
     double const dt                       = 0.05;
 
-    auto curtiss_hirschfelder = ponio::make_lawson_problem( k, n );
+    auto curtiss_hirschfelder = ponio::make_lawson_problem( l, n );
 
     auto exp = []( double x )
     {
@@ -83,6 +126,7 @@ TEST_CASE( "number_of_eval::exponential_runge_kutta" )
     std::size_t manual_counter = 0;
 
     double const k = 50;
+    double const l = -k;
     auto n         = [&, k]( double t, double )
     {
         ++manual_counter;
@@ -94,7 +138,7 @@ TEST_CASE( "number_of_eval::exponential_runge_kutta" )
     ponio::time_span<double> const t_span = { 0., 2. };
     double const dt                       = 0.05;
 
-    auto curtiss_hirschfelder = ponio::make_lawson_problem( k, n );
+    auto curtiss_hirschfelder = ponio::make_lawson_problem( l, n );
     auto sol_range            = ponio::make_solver_range( curtiss_hirschfelder, ponio::runge_kutta::exprk22(), y_0, t_span, dt );
     auto it_sol               = sol_range.begin();
 
@@ -108,6 +152,20 @@ TEST_CASE( "number_of_eval::exponential_runge_kutta" )
     CHECK( cumulative_counter == manual_counter );
 }
 
+/**
+ * ----------------------------------------------------------------------------
+ *
+ * # Diagonal Implicit Runge-Kutta method
+ *
+ * This class of methods are writing to solve a problem with form as
+ *
+ * \f$$
+ *  \dot{y} = f(t, y)
+ * \f$$
+ *
+ * we also need the Jacobian function of \f$f\$f (store as `df`) to implicit the problem, we build an object `curtiss_hirschfelder` to keep
+ * link between two mathematic objects.
+ */
 TEST_CASE( "number_of_eval::diagonal_implicit_runge_kutta" )
 {
     std::size_t manual_counter = 0;
@@ -116,7 +174,7 @@ TEST_CASE( "number_of_eval::diagonal_implicit_runge_kutta" )
     auto f         = [&, k]( double t, double y )
     {
         ++manual_counter;
-        return k * ( y + std::cos( t ) );
+        return k * ( std::cos( t ) - y );
     };
 
     auto df = [=]( double, double )
@@ -143,6 +201,20 @@ TEST_CASE( "number_of_eval::diagonal_implicit_runge_kutta" )
     CHECK( cumulative_counter == manual_counter );
 }
 
+/**
+ * ----------------------------------------------------------------------------
+ *
+ * # ROCK method
+ *
+ * This class of methods are writing to solve a problem with form as
+ *
+ * \f$$
+ *  \dot{y} = f(t, y)
+ * \f$$
+ *
+ * with \f$f\f$ an operator with large negative eigenvalues, this kind of methods has an adaptive number of stages, and needs more
+ * evaluation of function \f$f\f$ to estimate spectral radius.
+ */
 TEST_CASE( "number_of_eval::rock2" )
 {
     std::size_t manual_counter = 0;
@@ -151,7 +223,7 @@ TEST_CASE( "number_of_eval::rock2" )
     auto curtiss_hirschfelder = [&, k]( double t, double y )
     {
         ++manual_counter;
-        return k * ( y + std::cos( t ) );
+        return k * ( std::cos( t ) - y );
     };
 
     double y_0 = 2.0;
@@ -180,7 +252,7 @@ TEST_CASE( "number_of_eval::rock4" )
     auto curtiss_hirschfelder = [&, k]( double t, double y )
     {
         ++manual_counter;
-        return k * ( y + std::cos( t ) );
+        return k * ( std::cos( t ) - y );
     };
 
     double y_0 = 2.0;
@@ -201,6 +273,22 @@ TEST_CASE( "number_of_eval::rock4" )
     CHECK( cumulative_counter == manual_counter );
 }
 
+/**
+ * ----------------------------------------------------------------------------
+ *
+ * # PIROCK method
+ *
+ * This class of methods are writing to solve a problem with form as
+ *
+ * \f$$
+ *  \dot{y} = f(t, y) + g(t,y)
+ * \f$$
+ *
+ * with \f$f\f$ an operator with large negative eigenvalues, solved with ROCK2 method, and \f$g\f$ an operator which we would like solve
+ * with an implicit method (so we need the Jacobian function), we store respectively in `f_ex`, `f_im` (with its Jacobian `df_im`). We
+ * choose \f$f:t,y\mapsto k\cos(t)\f$, and \f$g:t,y\mapsto -k y\f$. We build an object `curtiss_hirschfelder` to keep link between three
+ * mathematic objects.
+ */
 TEST_CASE( "number_of_eval::pirock" )
 {
     std::size_t manual_counter_im = 0;
@@ -210,7 +298,7 @@ TEST_CASE( "number_of_eval::pirock" )
     auto f_im      = [&]( double, double y )
     {
         ++manual_counter_im;
-        return k * y;
+        return -k * y;
     };
     auto df_im = [&]( double, double )
     {
@@ -244,6 +332,25 @@ TEST_CASE( "number_of_eval::pirock" )
     CHECK( cumulative_counter_ex == manual_counter_ex );
 }
 
+/**
+ * ----------------------------------------------------------------------------
+ *
+ * # Splitting operator method
+ *
+ * This class of methods are writing to solve a problem with form as
+ *
+ * \f$$
+ *  \dot{y} = \sum_i f_i(t, y)
+ * \f$$
+ *
+ * We split the problem into some subproblem
+ *
+ * \f$$
+ *  \dot{y} = f_i(t, y)
+ * \f$$
+ *
+ * and solve consecutively each subproblem.
+ */
 TEST_CASE( "number_of_eval::splitting_lie" )
 {
     std::size_t manual_counter_1 = 0;
@@ -253,7 +360,7 @@ TEST_CASE( "number_of_eval::splitting_lie" )
     auto f1        = [&]( double, double y )
     {
         ++manual_counter_1;
-        return k * y;
+        return -k * y;
     };
     auto f2 = [&, k]( double t, double )
     {
@@ -294,7 +401,7 @@ TEST_CASE( "number_of_eval::splitting_strang" )
     auto f1        = [&]( double, double y )
     {
         ++manual_counter_1;
-        return k * y;
+        return -k * y;
     };
     auto f2 = [&, k]( double t, double )
     {
@@ -335,7 +442,7 @@ TEST_CASE( "number_of_eval::splitting_adaptive_strang" )
     auto f1        = [&]( double, double y )
     {
         ++manual_counter_1;
-        return k * y;
+        return -k * y;
     };
     auto f2 = [&, k]( double t, double )
     {
