5b7290893c1471c3c27384703456216d0f5be1d6
Josselin Massot <josselin.massot@polytechnique.edu>
2024-10-14 10:44:43 +0200

    add example visu

--- ponio/examples/exp_splitting.cpp
+++ ponio/examples/exp_splitting.cpp
@@ -18,8 +18,6 @@ int
 main()
 {
     std::string const dirname = "exp_splitting_data";
-    auto filename             = std::filesystem::path( dirname ) / "exp_splitting.dat";
-    observer::file_observer fobs( filename );
 
     double lambda = 0.3;
 
@@ -40,13 +38,37 @@ main()
         return { tn + dt, std::exp( lambda * dt ) * yn, dt };
     };
 
+    auto exact_solver = [=]( auto /* f */, double tn, double yn, double dt ) -> std::tuple<double, double, double>
+    {
+        return { tn + dt, std::exp( dt ) * yn, dt };
+    };
+
     double const y0 = 1.0;
-    double const dt = 0.1;
+    double const dt = 0.5;
 
-    auto strang = ponio::splitting::make_strang_tuple( std::make_pair( ponio::make_user_defined_method( exact_solver_f1 ), 0.005 ),
-        std::make_pair( ponio::runge_kutta::rk_44(), 0.005 ) );
+    // 3 examples
+    {
+        auto strang = ponio::splitting::make_strang_tuple( std::make_pair( ponio::make_user_defined_method( exact_solver_f1 ), dt ),
+            std::make_pair( ponio::runge_kutta::rk_22_ralston(), 0.5 * dt ) );
 
-    ponio::solve( pb, strang, y0, { 0., 2.0 }, dt, fobs );
+        auto filename = std::filesystem::path( dirname ) / "exp_strang.dat";
+        observer::file_observer fobs( filename );
+        ponio::solve( pb, strang, y0, { 0., 5.0 }, dt, fobs );
+    }
+    {
+        auto exact = ponio::make_user_defined_method( exact_solver );
+
+        auto filename = std::filesystem::path( dirname ) / "exp_exact.dat";
+        observer::file_observer fobs( filename );
+        ponio::solve( pb, exact, y0, { 0., 5.0 }, dt, fobs );
+    }
+    {
+        auto rk2 = ponio::runge_kutta::rk_22_ralston();
+
+        auto filename = std::filesystem::path( dirname ) / "exp_rk2.dat";
+        observer::file_observer fobs( filename );
+        ponio::solve( pb, rk2, y0, { 0., 5.0 }, dt, fobs );
+    }
 
     return 0;
 }
--- ponio/examples/exp_splitting.py
+++ ponio/examples/exp_splitting.py
@@ -20,15 +20,21 @@ args = [os.path.join(".", name)]
 process = subprocess.Popen(args)
 process.wait()
 
-data = np.loadtxt(os.path.join(data_dir, "exp_splitting.dat"))
+test_cases = [
+    ("strang", "Strang splitting with exact solver and RK(2,2) Ralston"),
+    ("exact", "exact solver"),
+    ("rk2", "RK(2,2) Ralston")
+]
+
+for tag, label in test_cases:
+    data = np.loadtxt(os.path.join(data_dir, f"exp_{tag}.dat"))
+
+    plt.plot(data[:, 0], data[:, 1], "-+", label=label)
 
 plt.plot(data[:, 0], np.exp(data[:, 0]), "--", label="exact solution")
-plt.plot(data[:, 0], data[:, 1], "-+",
-         label="solution with a Strang splitting with user defined solver")
-
 plt.legend()
 
 plt.savefig(os.path.join(data_dir, "01.png"))
 
-plt.title("$\dot{u} = u$, $u(t=0)=1$")
+plt.title(r"$\dot{u} = \lambda u + (1-\lambda) u$, $u(t=0)=1$")
 plt.show()
