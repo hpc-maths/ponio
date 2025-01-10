e3bd48d57f2d038dc6fd82dcb2d457955640ebdc
Josselin Massot <josselin.massot@polytechnique.edu>
2025-01-10 13:34:48 +0100

    improve compare

--- ponio/doc/source/compare/scipy/lorenz.py
+++ ponio/doc/source/compare/scipy/lorenz.py
@@ -1,5 +1,25 @@
 import numpy as np
+import scipy
 from scipy.integrate import solve_ivp
+
+
+class RK44(scipy.integrate._ivp.rk.RungeKutta):
+    order = 4
+    error_estimator_order = 4
+    n_stages = 4
+    C = np.array([0, 1/2, 1/2, 1])
+    A = np.array([
+        [0, 0, 0, 0],
+        [1/2, 0, 0, 0],
+        [0, 1/2, 0, 0],
+        [0, 0, 1, 0]
+    ])
+    B = np.array([1/6, 1/3, 1/3, 1/6])
+    E = np.array([0.1, 0.1, 0.1, 0.1, 0.1])
+    P = np.array([[1, -4 / 3, 5 / 9, 0],
+                  [0, 1, -2/3, 0],
+                  [0, 4/3, -8/9, 0],
+                  [0, -1, 1, 0]])
 
 
 def lorenz_system(t, y):
@@ -16,8 +36,9 @@ def lorenz_system(t, y):
 
 y0 = np.array([1., 1., 1.])
 t_span = [0., 20.]
+dt = 0.05
 
-sol = solve_ivp(lorenz_system, t_span, y0)
+sol = solve_ivp(lorenz_system, t_span, y0, method=RK44, first_step=dt)
 
 save_sol = np.zeros(shape=(4, len(sol.t)))
 save_sol[0, :] = sol.t
