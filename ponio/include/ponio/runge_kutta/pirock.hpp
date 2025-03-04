5388186af3166ccb5986047092c44f90f762e3cb
Josselin Massot <josselin.massot@polytechnique.edu>
2025-03-04 11:04:57 +0100

    remove dev tests

--- ponio/include/ponio/runge_kutta/pirock.hpp
+++ ponio/include/ponio/runge_kutta/pirock.hpp
@@ -428,9 +428,9 @@ namespace ponio::runge_kutta::pirock
                     u_np1 = us_s - err_D + 0.5 * dt * pb.implicit_part( tn, u_sp1 ) + 0.5 * dt * pb.implicit_part( tn, u_sp2 )
                           + dt / ( 2. - 4. * gamma ) * shampine_element;
 
-                    err_R.name() = "err_R";
-                    err_D.name() = "err_D";
-                    samurai::save( "err_R", un.mesh(), err_R, err_D );
+                    // err_R.name() = "err_R";
+                    // err_D.name() = "err_D";
+                    // samurai::save( "err_R", un.mesh(), err_R, err_D );
 
                     _info.absolute_tolerance = 1e-4;
                     _info.relative_tolerance = 1e-4;
@@ -454,7 +454,8 @@ namespace ponio::runge_kutta::pirock
                         static_cast<value_t>( 0. ),
                         accumulator_error_gen( un.array(), u_np1.array(), _info.absolute_tolerance, _info.relative_tolerance ) );
 
-                    std::cout << "err_D = " << err_D_scalar << " err_R = " << err_R_scalar << std::endl;
+                    // std::cout << "err_D = " << err_D_scalar << " err_R = " << err_R_scalar << std::endl;
+
                     _info.error   = std::max( err_D_scalar, err_R_scalar );
                     _info.success = _info.error < 1.0;
 
