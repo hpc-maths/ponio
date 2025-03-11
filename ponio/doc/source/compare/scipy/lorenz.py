e3bd5b1f943856c856140b66f289048899f5c689
Josselin Massot <josselin.massot@polytechnique.edu>
2025-03-11 14:38:39 +0100

    change tolerance in scipy to get same results

--- ponio/doc/source/compare/scipy/lorenz.py
+++ ponio/doc/source/compare/scipy/lorenz.py
@@ -5,7 +5,7 @@ from scipy.integrate import solve_ivp
 
 class RK44(scipy.integrate._ivp.rk.RungeKutta):
     order = 4
-    error_estimator_order = 4
+    error_estimator_order = 1
     n_stages = 4
     C = np.array([0, 1/2, 1/2, 1])
     A = np.array([
@@ -38,7 +38,8 @@ y0 = np.array([1., 1., 1.])
 t_span = [0., 20.]
 dt = 0.05
 
-sol = solve_ivp(lorenz_system, t_span, y0, method=RK44, first_step=dt)
+sol = solve_ivp(lorenz_system, t_span, y0, method=RK44,
+                first_step=dt, max_step=dt, rtol=1.0, atol=1.0)
 
 save_sol = np.zeros(shape=(4, len(sol.t)))
 save_sol[0, :] = sol.t
