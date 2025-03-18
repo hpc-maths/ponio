c1a57444a2fed7920ae3077dc3de89a33e724cc9
Josselin Massot <7198360+kivvix@users.noreply.github.com>
2025-03-18 12:55:56 +0100

    feat: change default build to -DBUILD_OFFLINE (#80)
    <!-- Thank you for your contribution to ponio! -->
    <!-- ༊彡 -->
    
    ## Description
    <!-- A clear and concise description of what you have done in this PR.
    -->
    Change default build to offline (no request for doi).

--- ponio/doc/scripts/pre_install.sh
+++ ponio/doc/scripts/pre_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # generate ponio/runge_kutta/butcher_methods.hpp file
-cmake . -B ./build -G "Ninja Multi-Config" -DBUILD_DOC=ON
+cmake . -B ./build -G "Ninja Multi-Config" -DBUILD_DOC=ON -DBUILD_OFFLINE=OFF
 cmake --build ./build --config Release
 
 # convert notebooks into rst files for documentation
