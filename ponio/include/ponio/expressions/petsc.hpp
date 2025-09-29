d9d69df1be33e45f3302e975b44d23ee8640bfd0
Josselin Massot <josselin.massot@polytechnique.edu>
2025-09-29 15:31:09 +0200

    add specialization for PETSc Vec

--- ponio/include/ponio/expressions/petsc.hpp
+++ ponio/include/ponio/expressions/petsc.hpp
@@ -118,4 +118,119 @@ namespace ponio::expression
             VecRestoreArrayRead( _data, &_reader_data );
         }
     };
+
+    template <>
+    struct state<Mat>
+    {
+        static constexpr bool is_ponio_expression = true;
+
+        using container_type = Mat;
+        container_type& _data; // store only a reference on data
+        double const* _reader_data = nullptr;
+
+        state( container_type& dat )
+            : _data( dat )
+        {
+        }
+
+        auto
+        operator[]( std::size_t idx ) const
+        {
+            if ( _reader_data == nullptr )
+            {
+                int nb_elm = 1;
+
+                int m, n;
+                MatGetSize( _data, &m, &n );
+
+                // only for row-oriented dense matrix
+                int idxm = static_cast<int>( idx / m );
+                int idxn = static_cast<int>( idx % n );
+
+                double data_i;
+
+                MatGetValues( _data, nb_elm, &idxm, nb_elm, &idxn, &data_i );
+
+                return data_i;
+            }
+
+            return _reader_data[idx];
+        }
+
+        template <typename expression_t>
+            requires detail::is_ponio_expression<expression_t>
+        state&
+        operator=( expression_t& expr )
+        {
+            double* _ptr_data;
+            MatDenseGetArray( _data, &_ptr_data );
+
+            expr.raw_data_open();
+
+            for ( std::size_t i = 0ul; i < expr.size(); ++i )
+            {
+                _ptr_data[i] = expr[i];
+            }
+
+            expr.raw_data_close();
+
+            MatDenseRestoreArray( _data, &_ptr_data );
+
+            return *this;
+        }
+
+        template <typename expression_t>
+            requires detail::is_ponio_expression<expression_t>
+        state&
+        operator=( expression_t&& expr )
+        {
+            double* _ptr_data;
+            MatDenseGetArray( _data, &_ptr_data );
+
+            expr.raw_data_open();
+
+            for ( std::size_t i = 0ul; i < expr.size(); ++i )
+            {
+                _ptr_data[i] = expr[i];
+            }
+
+            expr.raw_data_close();
+
+            MatDenseRestoreArray( _data, &_ptr_data );
+
+            return *this;
+        }
+
+        std::size_t
+        size() const
+        {
+            int m, n;
+            MatGetSize( _data, &m, &n );
+            return static_cast<std::size_t>( m * n );
+        }
+
+        container_type const&
+        data() const
+        {
+            return _data;
+        }
+
+        container_type&
+        data()
+        {
+            return _data;
+        }
+
+        void
+        raw_data_open()
+        {
+            MatDenseGetArrayRead( _data, &_reader_data );
+        }
+
+        void
+        raw_data_close()
+        {
+            MatDenseRestoreArrayRead( _data, &_reader_data );
+        }
+    };
 } // namespace ponio::expression
