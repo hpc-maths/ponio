343e494badcfa8b2153e2e35f00702b0e6ee310f
Josselin Massot <josselin.massot@polytechnique.edu>
2026-01-12 15:57:22 +0100

    add pair of Butcher tableaus

--- ponio/include/ponio/detail.hpp
+++ ponio/include/ponio/detail.hpp
@@ -285,6 +285,65 @@ namespace ponio::detail
         return power_impl( std::forward<Arithmetic_t>( value ), std::make_index_sequence<Iexp>() );
     }
 
+    /**
+     * @brief join id of a list of Butcher tableaus
+     *
+     * @tparam tableau_ts list of Butcher tableaus type
+     */
+    template <typename... tableau_ts>
+    struct join_id
+    {
+        using size       = std::integral_constant<std::size_t, ( tableau_ts::id.size() + ... + 0 )>;
+        using c_str_type = std::array<char, size() + 1>;
+
+        static constexpr c_str_type
+        join_as_array() noexcept
+        {
+            c_str_type data{};
+
+            auto append = [i = 0, &data]( auto const& id ) mutable
+            {
+                for ( auto character : id )
+                {
+                    data[i++] = character;
+                }
+            };
+
+            ( append( tableau_ts::id ), ... );
+
+            data[size()] = 0; // last character is null like c-string
+            return data;
+        }
+
+        static constexpr c_str_type c_str = join_as_array();
+        static constexpr std::string_view value{ c_str.data(), size() };
+    };
+
+    /**
+     * @brief helper for join_id structure
+     *
+     * @tparam tableau_ts list of Butcher tableaus type
+     */
+    template <typename... tableau_ts>
+    static constexpr auto join_id_v = join_id<tableau_ts...>::value;
+
+    /**
+     * @brief just a classical separator in a constant expression variable
+     *
+     */
+    static constexpr std::string_view underscore = "_";
+
+    /**
+     * @brief a simple separator for join_id structure
+     *
+     * @tparam sep string_view to use as separator
+     */
+    template <std::string_view const& sep = underscore>
+    struct separator
+    {
+        static constexpr std::string_view id = sep;
+    };
+
     // some concepts
     template <typename T>
     concept has_identity_method = std::is_member_function_pointer_v<decltype( &T::identity )>;
