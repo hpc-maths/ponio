b090602d38865e16aee30c8030fb43b95e9720fc
Josselin Massot <josselin.massot@polytechnique.edu>
2026-01-26 15:49:08 +0100

    fix IWYU pragma

--- ponio/include/ponio/runge_kutta/dirk.hpp
+++ ponio/include/ponio/runge_kutta/dirk.hpp
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// IWYU pragma: private, include "../runge_kutta.hpp"
+// IWYU pragma: private
 
 #pragma once
 
