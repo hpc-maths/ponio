647121d4132a63d17fe3516c2cb70c7e74e8c585
Josselin Massot <josselin.massot@polytechnique.edu>
2026-07-21 11:19:22 +0200

    close file in file_observer

--- ponio/include/ponio/observer.hpp
+++ ponio/include/ponio/observer.hpp
@@ -93,6 +93,10 @@ namespace ponio::observer
     {
         using derived_t = Derived_t;
 
+        virtual ~base_observer()
+        {
+        }
+
         template <typename state_t, typename value_t>
         void
         operator()( value_t tn, state_t const& un, value_t dt );
@@ -156,6 +160,8 @@ namespace ponio::observer
 
         file_observer( std::filesystem::path const& path );
 
+        ~file_observer();
+
         // file_observer( file_observer const& ) = delete;
 
       private:
@@ -173,6 +179,15 @@ namespace ponio::observer
     file_observer<char_t>::file_observer( std::filesystem::path const& path )
         : out( create_directory_if_needed( path ) )
     {
+    }
+
+    /**
+     * @brief close file properly
+     */
+    template <typename char_t>
+    file_observer<char_t>::~file_observer()
+    {
+        out.close();
     }
 
     /**
